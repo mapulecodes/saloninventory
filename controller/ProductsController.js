cee95779fdf1875aab6e148658d8e720597a92f7
mapulecodes <mbuzwanamapule@gmail.com>
2024-09-04 15:21:43 +0200

    needs fix

--- controller/ProductsController.js
+++ controller/ProductsController.js
@@ -14,48 +14,48 @@ productsRouter.get("/", (req, res) => {
   }
 });
 
-// productsRouter.get("/:id", (req, res) => {
-//   try {
-//     product.fetchProduct(req, res);
-//   } catch (e) {
-//     res.json({
-//       status: res.statusCode,
-//       msg: "Failed to get a product",
-//     });
-//   }
-// });
+productsRouter.get("/:id", (req, res) => {
+  try {
+    product.fetchProduct(req, res);
+  } catch (e) {
+    res.json({
+      status: res.statusCode,
+      msg: "Failed to get a product",
+    });
+  }
+});
 
-// productsRouter.post("/addProduct", (req, res) => {
-//   try {
-//     Products.addProduct(req, res);
-//   } catch (e) {
-//     res.json({
-//       status: res.statusCode,
-//       msg: "Failed to add a new product.",
-//     });
-//   }
-// });
+productsRouter.post("/addProduct", (req, res) => {
+  try {
+    Products.addProduct(req, res);
+  } catch (e) {
+    res.json({
+      status: res.statusCode,
+      msg: "Failed to add a new product.",
+    });
+  }
+});
 
-// productsRouter.patch("/update/:id", (req, res) => {
-//   try {
-//     Products.updateProduct(req, res);
-//   } catch (e) {
-//     res.json({
-//       status: res.statusCode,
-//       msg: "Failed to update a product.",
-//     });
-//   }
-// });
+productsRouter.patch("/update/:id", (req, res) => {
+  try {
+    Products.updateProduct(req, res);
+  } catch (e) {
+    res.json({
+      status: res.statusCode,
+      msg: "Failed to update a product.",
+    });
+  }
+});
 
-// productsRouter.delete("/delete/:id", (req, res) => {
-//   try {
-//     Products.deleteProduct(req, res);
-//   } catch (e) {
-//     res.json({
-//       status: res.statusCode,
-//       msg: "Failed to delete a product.",
-//     });
-//   }
-// });
+productsRouter.delete("/delete/:id", (req, res) => {
+  try {
+    Products.deleteProduct(req, res);
+  } catch (e) {
+    res.json({
+      status: res.statusCode,
+      msg: "Failed to delete a product.",
+    });
+  }
+});
 
 export { productsRouter };
---
+++ model/user.js
@@ -0,0 +1,76 @@
+import { db } from "../config/index.js";
+
+class Users {
+    async fetchUsers(req, res) {
+        try {
+            const strQry = `
+                SELECT
+                    ID,
+                    UserName,
+                    UserRole,
+                    UserEmail
+                FROM users;
+            `;
+            db.query(strQry, (err, results) => {
+                if (err)
+                    return res.status(500).json({
+                        status: res.statusCode,
+                        message: err.message,
+                    });
+
+                if (results.length === 0) {
+                    return res.status(404).json({ 
+                        status: res.statusCode, 
+                        message: "No users found" 
+                    });
+                }
+
+                res.status(200).json({ 
+                    status: res.statusCode, 
+                    results 
+                });
+            });
+        } catch (e) {
+            res.status(404).json({
+                status: res.statusCode,
+                msg: e.message,
+            });
+        }
+    }
+
+    async addUser(req, res) {
+        try {
+            const strQry = `
+                INSERT INTO users SET ?;
+            `;
+            const userData = {
+                UserName: req.body.UserName,
+                UserRole: req.body.UserRole,
+                UserEmail: req.body.UserEmail,
+                UserPassword: req.body.UserPassword,
+            };
+
+            db.query(strQry, userData, (err) => {
+                if (err)
+                    return res.status(500).json({
+                        status: res.statusCode,
+                        message: "Unable to add a new user",
+                    });
+
+                res.json({
+                    status: res.statusCode,
+                    msg: "User was added successfully",
+                });
+            });
+        } catch (e) {
+            res.status(404).json({
+                status: res.statusCode,
+                msg: e.message,
+            });
+        }
+    }
+}
+
+const users = new Users();
+
+export default users;
